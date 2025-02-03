3cd2fe7ca60808df494661f93032bcfee54b138c
mikrukovd <z123456689z@gmail.com>
2025-02-03 12:20:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import requests
 
 
-def weather(place):
-    url = 'https://wttr.in/{}?nTqmM&lang=ru'.format(place)
-    responce = requests.get(url)
-    responce.raise_for_status()
-    return print(responce.text)
+def get_weather(city):
+    params = {
+        'nTqmM': '',
+        'lang': 'ru'
+    }
+    url = 'https://wttr.in/{}'.format(city)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text
 
 
 def main():
-    city_lst = ['Лондон', 'Шереметьево', 'Череповец']
-    for city in city_lst:
-        weather(city)
+    cities = ['Лондон', 'Шереметьево', 'Череповец']
+    for city in cities:
+        print(get_weather(city))
 
 
 if __name__ == "__main__":
